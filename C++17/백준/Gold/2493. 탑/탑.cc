8e3fad040faf512a872299587a662fa1dd62551d
TAEJIN KIM <ktj503@naver.com>
2026-03-12 17:20:39 +0900

    [Gold V] Title: 탑, Time: 284 ms, Memory: 10136 KB -BaekjoonHub

---
+++ C++17/백준/Gold/2493. 탑/탑.cc
@@ -0,0 +1,31 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> tops(n, 0);
+    for(int i = 0; i < n; i++){
+        cin >> tops[i];
+    }
+    
+    // 높이, 번호
+    vector<pair<int, int>> st;
+    
+    for(int i = 0; i < n; i++){
+        while(!st.empty() && st.back().first < tops[i]){
+            st.pop_back();
+        }
+        if(st.empty()){
+            cout << 0 << " ";
+        }else{
+            cout << st.back().second << " ";
+        }
+        st.push_back({tops[i], i+1});
+    }
+    
+    return 0;
+    
+}
