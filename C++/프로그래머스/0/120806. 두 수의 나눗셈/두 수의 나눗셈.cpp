cdffd0db211f260196f148c9a39d7177f4111967
TAEJIN KIM <ktj503@naver.com>
2026-03-31 21:09:56 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    double answer = 0;
+    answer = (double) num1 / (double) num2 * 1000;
+    return (int)answer;
+}
