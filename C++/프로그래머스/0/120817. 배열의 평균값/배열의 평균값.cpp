2f450126838e79e7e8e03a42cac7183fd7ce8f73
TAEJIN KIM <ktj503@naver.com>
2026-04-01 14:28:49 +0900

    [level 0] Title: 배열의 평균값, Time: 0.03 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/120817. 배열의 평균값/배열의 평균값.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+#include <numeric>
+
+using namespace std;
+
+double solution(vector<int> numbers) {
+    
+    int sum = accumulate(numbers.begin(), numbers.end(), 0);
+    
+    return (double)sum / (double)numbers.size();
+    
+}
