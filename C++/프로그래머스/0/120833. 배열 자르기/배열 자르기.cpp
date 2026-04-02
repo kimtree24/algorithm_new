77f6c29f543f61de3505c45a45afa055e7faf37c
TAEJIN KIM <ktj503@naver.com>
2026-04-02 14:38:32 +0900

    [level 0] Title: 배열 자르기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/120833. 배열 자르기/배열 자르기.cpp
@@ -0,0 +1,9 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> numbers, int num1, int num2) {
+    
+    return vector<int> (numbers.begin() + num1, numbers.begin() + num2 + 1);
+}
