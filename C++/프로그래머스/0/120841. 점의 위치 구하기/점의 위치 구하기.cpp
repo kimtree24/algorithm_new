d9b1f54bd54cbed2a9c8f9d7bca5312c777eeb84
TAEJIN KIM <ktj503@naver.com>
2026-04-03 15:07:52 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> dot) {
+    if(dot[0] > 0 && dot[1] > 0){
+        return 1;
+    }else if(dot[0] > 0 && dot[1] < 0){
+        return 4;
+    }else if(dot[0] < 0 && dot[1] < 0){
+        return 3;
+    }else{
+        return 2;
+    }
+}
