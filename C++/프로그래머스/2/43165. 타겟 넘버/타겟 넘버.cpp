ff36d3906430069b33ee271924ed47cec5e92f6e
TAEJIN KIM <ktj503@naver.com>
2026-04-04 22:03:16 +0900

    [level 2] Title: 타겟 넘버, Time: 4.34 ms, Memory: 3.62 MB -BaekjoonHub

--- C++/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ C++/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -1,37 +1,27 @@
+#include <string>
 #include <vector>
 
 using namespace std;
 
-void dfs(vector<int> &numbers, int target, int cur, vector<int> &ans, int idx){
-    // 탈출
-    if(idx == numbers.size() && cur == target){
-        ans[0] += 1;
+void dfs(vector<int> &numbers, int target, int cur, int idx, vector<int> &ans){
+    if(idx == numbers.size()){
+        if(cur == target){
+            ans[0] += 1;
+        }
         return;
     }
-
     
-    if(idx < numbers.size()){
-        // 다음 마이너스 가지 수행
-        cur -= numbers[idx];
-        dfs(numbers, target, cur, ans, idx + 1);
-        cur += numbers[idx];
-    
-        // 다음 플러스 가지 수행
-        cur += numbers[idx];
-        dfs(numbers, target, cur, ans, idx + 1);
-        cur -= numbers[idx];
-    }
-    
-    return;
+    dfs(numbers, target, cur + numbers[idx], idx + 1, ans);
+    dfs(numbers, target, cur - numbers[idx], idx + 1, ans);
     
 }
 
-int solution(vector<int> numbers, int target){
-    int cur = 0;
-    vector<int> ans = {0};
+int solution(vector<int> numbers, int target) {
     
-    dfs(numbers, target, cur, ans, 0);
+    vector<int> ans(1,0);
+    
+    dfs(numbers, target, 0, 0, ans);
     
     return ans[0];
-        
+    
 }
