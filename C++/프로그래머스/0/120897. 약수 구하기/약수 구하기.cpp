f39b2b4489f1896f3490ae72435d66ed9dcee5db
TAEJIN KIM <ktj503@naver.com>
2026-04-09 14:42:16 +0900

    [level 0] Title: 약수 구하기, Time: 0.05 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/120897. 약수 구하기/약수 구하기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n) {
+    vector<int> ans;
+    
+    for(int i = 1; i <= n; i++){
+        if(n % i == 0){
+            ans.push_back(i);
+        }
+    }
+    
+    return ans;
+}
