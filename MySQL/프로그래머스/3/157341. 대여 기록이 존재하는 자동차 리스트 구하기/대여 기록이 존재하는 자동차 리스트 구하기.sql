f09b35e34584640dc7e39c532e7ad3591999a65d
TAEJIN KIM <ktj503@naver.com>
2026-04-26 14:29:20 +0900

    [level 3] Title: 대여 기록이 존재하는 자동차 리스트 구하기, Time: , Memory: undefined -BaekjoonHub

--- MySQL/프로그래머스/3/157341. 대여 기록이 존재하는 자동차 리스트 구하기/대여 기록이 존재하는 자동차 리스트 구하기.sql
+++ MySQL/프로그래머스/3/157341. 대여 기록이 존재하는 자동차 리스트 구하기/대여 기록이 존재하는 자동차 리스트 구하기.sql
@@ -1,5 +1,8 @@
-select distinct(a.car_id)
-from car_rental_company_car as a
-join car_rental_company_rental_history as b on a.car_id = b.car_id
-where a.car_type = '세단' and month(b.start_date) = 10
-order by a.car_id desc
+SELECT DISTINCT c.car_id
+FROM car_rental_company_car c
+JOIN car_rental_company_rental_history h
+    ON c.car_id = h.car_id
+WHERE c.car_type = '세단'
+AND h.start_date >= '2022-10-01'
+AND h.start_date <  '2022-11-01'
+ORDER BY c.car_id desc
