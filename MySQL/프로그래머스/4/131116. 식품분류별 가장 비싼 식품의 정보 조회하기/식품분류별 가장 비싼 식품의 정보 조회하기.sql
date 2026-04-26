a5eac34344a7ecd9f52750fd93dfedce9da1ba61
TAEJIN KIM <ktj503@naver.com>
2026-04-26 12:16:31 +0900

    [level 4] Title: 식품분류별 가장 비싼 식품의 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

--- MySQL/프로그래머스/4/131116. 식품분류별 가장 비싼 식품의 정보 조회하기/식품분류별 가장 비싼 식품의 정보 조회하기.sql
+++ MySQL/프로그래머스/4/131116. 식품분류별 가장 비싼 식품의 정보 조회하기/식품분류별 가장 비싼 식품의 정보 조회하기.sql
@@ -1,11 +1,10 @@
 select category, price as max_price, product_name
 from(
-    select *,
-        row_number() over(
+select *,
+        row_number() over (
             partition by category
-            order by price desc
-        ) as rn
-    from food_product
+            order by price desc) as rn
+from food_product
 ) as t
 where rn = 1 and category in ('과자', '국', '김치', '식용유')
-order by max_price desc;
+order by max_price desc
